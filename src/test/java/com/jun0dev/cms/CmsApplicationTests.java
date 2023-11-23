f227098f1a9a4ce77c44b73dc987c7556155acc9
jun0 <jun0dev@icloud.com>
2023-11-23 20:46:35 +0900

    Initial commit

---
+++ src/test/java/com/jun0dev/cms/CmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jun0dev.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
