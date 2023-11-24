817fee3850209f89f43a988fc8cd3c8510a68b35
jun0 <jun0dev@icloud.com>
2023-11-24 23:44:38 +0900

    feat : [003-EMAIL_VERIFICATION] 커스텀 이메일 verification

--- user-api/src/main/java/com/jun0dev/cms/user/client/repository/CustomerRepository.java
+++ user-api/src/main/java/com/jun0dev/cms/user/client/repository/CustomerRepository.java
@@ -3,5 +3,9 @@ package com.jun0dev.cms.user.client.repository;
 import com.jun0dev.cms.user.client.domain.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+
 }
